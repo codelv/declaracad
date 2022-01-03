f24a6fb775ea565f2deaee000ff7996f21ff54e2
frmdstryr <frmdstryr@protonmail.com>
2022-01-03 15:59:43 -0500

    cnc: Add climb milling option to pocket and add cnc test

---
+++ tests/test_6_cnc.py
@@ -0,0 +1,23 @@
+"""
+Copyright (c) 2022, Jairus Martin.
+
+Distributed under the terms of the GPL v3 License.
+
+The full license is in the file LICENSE, distributed with this software.
+
+"""
+import os
+import pytest
+from OCCT.TopoDS import TopoDS_Shape
+from declaracad.occ.api import load_model
+from declaracad.occ.impl.occ_factories import OCC_FACTORIES  # Make sure it installs
+
+
+@pytest.mark.parametrize("name", os.listdir('examples/cnc/'))
+def test_cnc_examples(qt_app, name):
+    path = f"examples/cnc/{name}"
+    example = os.path.splitext(name)[0]
+    assembly = load_model(path)
+    for shape in assembly:
+        shape.render()
+    assert os.path.exists(f'{example}.nc')
