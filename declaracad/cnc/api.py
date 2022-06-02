b8d40564d2ff5ff9b81e4bd3f120f853f9e37e3c
frmdstryr <frmdstryr@protonmail.com>
2022-06-02 16:24:18 -0400

    Add contour toolpath

--- declaracad/cnc/api.py
+++ declaracad/cnc/api.py
@@ -5,6 +5,7 @@ from declaracad.cnc.operation import Operation
 with enaml.imports():
     from declaracad.cnc.cutters import ChamferMill, EndMill, FaceMill, Tool, TwistDrill
     from declaracad.cnc.operations.chamfer import ChamferOperation
+    from declaracad.cnc.operations.contour import ContourOperation
     from declaracad.cnc.operations.drilling import DrillingCycle
     from declaracad.cnc.operations.facing import FacingOperation
     from declaracad.cnc.operations.job import Job, JobSimulation
