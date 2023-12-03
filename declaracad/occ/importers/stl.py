38105ad12472de37ec229e2b1998c505906e484b
frmdstryr <frmdstryr@protonmail.com>
2023-12-03 18:34:26 -0500

    Add stl importer merge angle

--- declaracad/occ/importers/stl.py
+++ declaracad/occ/importers/stl.py
@@ -9,6 +9,7 @@ Created on Aug 31, 2020
 
 @author: jrm
 """
+from math import pi
 from OCCT.BRep import BRep_Builder
 from OCCT.RWStl import RWStl
 from OCCT.TopoDS import TopoDS_Face
@@ -16,11 +17,24 @@ from OCCT.TopoDS import TopoDS_Face
 from declaracad.occ.api import TopoShape
 
 
-def load_stl(filename):
-    """Load a stl model"""
+def load_stl(filename: str, merge_angle: float = pi/360) -> list[TopoShape]:
+    """Load a stl model
+
+    Parameters
+    ----------
+    filename: str
+        Path to stl file
+    merge_angle: float
+        maximum angle in radians between triangles to merge equal nodes
+
+    Returns
+    -------
+    result: list[TopoDS_Shape]
+        Loaded shape
+    """
     builder = BRep_Builder()
     shape = TopoDS_Face()
     builder.MakeFace(shape)
-    poly = RWStl.ReadFile_(filename)
+    poly = RWStl.ReadFile_(filename, merge_angle)
     builder.UpdateFace(shape, poly)
     return [TopoShape(shape=shape)]
