bd88adf92e753ce786fb71e407678c32a610f36d
frmdstryr <frmdstryr@protonmail.com>
2025-06-14 12:47:12 -0400

    Export stl with parallel=True

--- declaracad/occ/exporters/stl/exporter.py
+++ declaracad/occ/exporters/stl/exporter.py
@@ -67,7 +67,7 @@ class StlExporter(ModelExporter):
         # TODO: pyOCCT needs to support updating the reference
         # exporter.SetASCIIMode(not self.binary)
         mesh = BRepMesh_IncrementalMesh(
-            compound, self.linear_deflection, self.relative, self.angular_deflection
+            compound, self.linear_deflection, self.relative, self.angular_deflection, True
         )
         mesh.Perform()
         if not mesh.IsDone():
