229b6b3909093c30419add64f57fe00d0d2e2c2f
frmdstryr <frmdstryr@protonmail.com>
2026-03-02 16:10:15 -0500

    Fix pytest skipif warning

--- tests/test_6_cnc.py
+++ tests/test_6_cnc.py
@@ -16,7 +16,7 @@ from declaracad.occ.api import load_model
 is_ci = "CI" in os.environ
 
 
-@pytest.mark.skipIf(is_ci, "Disabled in CI")
+@pytest.mark.skipif(is_ci, "Disabled in CI")
 @pytest.mark.parametrize("name", os.listdir("examples/cnc/"))
 def test_cnc_examples(qt_app, name: str):
     path = f"examples/cnc/{name}"
